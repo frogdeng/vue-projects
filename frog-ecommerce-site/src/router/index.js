8ba91789c9576c52e0226872d558350bab2a3ee2
albert deng <albertdeng@kkbox.com>
2020-01-07 17:45:05 +0800

    login page

--- frog-ecommerce-site/src/router/index.js
+++ frog-ecommerce-site/src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Login from '@/components/pages/Login.vue'
+import Products from '@/components/pages/Products.vue'
+
 
 Vue.use(VueRouter)
 
@@ -17,6 +20,16 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/products',
+    name: 'Products',
+    component: Products
   }
 ]
 
